eb1aa3cbb64a762ba46a5e0639de40f6a167d422
Marc Went <marc.went+git@coachhub.com>
2024-11-28 11:43:45 +0100

    favicon & manifest

---
+++ app/manifest.ts
@@ -0,0 +1,32 @@
+import type { MetadataRoute } from "next";
+
+export const dynamic = "force-static";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Local Notes",
+    short_name: "Local Notes",
+    description: "Local Notes",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons: [
+      {
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
+      },
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+// const {
+//   PHASE_DEVELOPMENT_SERVER,
+//   PHASE_PRODUCTION_BUILD,
+// } = require("next/constants");
+
+import { PHASE_PRODUCTION_BUILD } from "next/constants.js";
+
+export default (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_BUILD) {
+    return {
+      output: "export",
+      basePath: "/local-notes",
+      images: {
+        unoptimized: true,
+      },
+    };
+  }
+
+  return {
+    output: "export",
+    images: {
+      unoptimized: true,
+    },
+  };
+};
